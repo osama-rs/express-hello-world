cb3e92d2644e28e78d9965a415c68b4701ac9298
osama bin junaid <osama.junaid@c3m.io>
2022-11-13 22:31:59 +0530

    changed dest

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+pkill node2
+echo $?
+if [ $? -ne 1 ]; then
+    echo "Application was stopped"
+fi
